c7774a8cd2ec9ea734cdcdfb7d10842b3bd99843
Andrey980 <andreymcunhasilva@gmail.com>
2023-08-03 23:47:02 -0300

    Update

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User
 from .models import Product
 
 
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+
+        return name
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
