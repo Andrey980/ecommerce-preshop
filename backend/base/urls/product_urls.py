3381f448641e3e1011c35590e9a87ad02749fd7d
Andrey980 <andreymcunhasilva@gmail.com>
2023-08-14 23:07:42 -0300

    Update

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from base.views import product_views as views
 
 urlpatterns = [
-    path('/', views.getProducts, name="products"),
-    path('/<str:pk>/', views.getProduct, name="product"),
+    path('', views.getProducts, name="products"),
+    path('<str:pk>/', views.getProduct, name="product"),
 ]
--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from base.views import user_views as views
 
 urlpatterns = [
-    path('/login/', views.MyTokenObtainPairView.as_view(),
+    path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
 
-    path('/register/', views.registerUser, name='register '),
+    path('register/', views.registerUser, name='register '),
 
-    path('/profile/', views.getUserProfile, name="users-profile"),
-    path('/', views.getUsers, name="users"),
+    path('profile/', views.getUserProfile, name="users-profile"),
+    path('', views.getUsers, name="users"),
 
 
 ]
